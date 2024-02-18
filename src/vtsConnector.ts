6006aace55d7cc9715a435a57d423b1f46978924
Renpona <renponaunit6@gmail.com>
2024-02-17 21:18:16 -0500

    vts connection minor updates

--- src/vtsConnector.ts
+++ src/vtsConnector.ts
@@ -5,29 +5,33 @@ const vts = require("vtubestudio");
 const ApiClient = vts.ApiClient;
 
 function setAuthToken(authenticationToken) {
-  // store the authentication token somewhere
-  fs.writeFileSync("./auth-token.txt", authenticationToken, {
-    encoding: "utf-8",
-  });  
+    // store the authentication token somewhere
+    fs.writeFileSync("./auth-token.txt", authenticationToken, {
+        encoding: "utf-8",
+    });  
 }
 
 function getAuthToken() {
-  // retrieve the stored authentication token
-  return fs.readFileSync("./auth-token.txt", "utf-8");
+    // retrieve the stored authentication token
+    return fs.readFileSync("./auth-token.txt", "utf-8");
 }
 
 const options = {
-  authTokenGetter: getAuthToken,
-  authTokenSetter: setAuthToken,
-  pluginName: "VTCursed",
-  pluginDeveloper: "Renpona",
-  webSocketFactory: (url) => new ws(url),
+    authTokenGetter: getAuthToken,
+    authTokenSetter: setAuthToken,
+    pluginName: "VTCursed",
+    pluginDeveloper: "Renpona",
+    webSocketFactory: (url) => new ws(url),
 
-  // Optionally set the URL or port to connect to VTube Studio at; defaults are as below:
+    // Optionally set the URL or port to connect to VTube Studio at; defaults are as below:
 
-  //TODO: add a way to take in host/port info. command line? config file? idk
-  //port: 8001,
-  //url: "ws://localhost:${port}",
+    //TODO: add a way to take in host/port info. command line? config file? idk
+    //port: 8001,
+    //url: "ws://localhost:${port}",
 };
 
-const apiClient = new ApiClient(options);
+function connectToVts() {
+    const apiClient = new ApiClient(options);
+    return apiClient;
+}
+
