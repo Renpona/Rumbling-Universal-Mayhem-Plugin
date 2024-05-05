e2f7b7dbba45b2828cc703a49416e1b6252de93d
Renpona <renponaunit6@gmail.com>
2024-05-05 13:34:18 -0400

    add fetch support

--- src/mtionConnector.ts
+++ src/mtionConnector.ts
@@ -9,9 +9,11 @@ const category = FormType.Vtuber;
 class ConnectorMtion implements VtuberSoftware {
     software: Protocol = Protocol.Mtion;
     isConnected: boolean = false;
+    logger = getLogger();
 
     connect(host: string, port: number) {
-        //const list = fetch(`http://${host}:${port}/external-trigger/triggers`);
+        const list = fetch(`http://${host}:${port}/external-trigger/triggers`).then((response) => {return response.json()});
+        this.logger.debug(list);
     };
 
     disconnect() {
