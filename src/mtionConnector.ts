a77c1f0ca044011414d02f12423273de73a7d4aa
Renpona <renponaunit6@gmail.com>
2024-05-05 15:11:29 -0400

    switch from fetch to Node HTTP

--- src/mtionConnector.ts
+++ src/mtionConnector.ts
@@ -2,7 +2,7 @@ import { ConnectionStatus, FormType, Protocol } from "./enums";
 import { MtionAction, VtuberSoftware } from "./types";
 import { updateStatus } from "./electron/electronMain";
 import { getLogger } from "./loggerConfig";
-
+import http from 'node:http';
 
 const category = FormType.Vtuber;
 
@@ -12,19 +12,20 @@ class ConnectorMtion implements VtuberSoftware {
     logger = getLogger();
 
     connect(host: string, port: number) {
-        const list = fetch(`http://${host}:${port}/external-trigger/triggers`).then((response) => {return response.json()});
-        this.logger.debug(list);
+        http.request(`${host}:${port}`);
     };
 
     disconnect() {
-        
+        return;
     };
 
     sendData(param: string, value: number) {
-        
+        return;
     };
 
     registerActions(action: MtionAction[]) {
-        
+        return;
     };
 }
+
+export { ConnectorMtion }
